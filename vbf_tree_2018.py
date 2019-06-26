e7aec78c3147d77dad448691b65ededf5c16d8b2
Alp Akpinar <aakpinar@bu.edu>
2019-06-26 18:11:52 -0500

    First commit

---
+++ vbf_tree_2018.py
@@ -0,0 +1,113 @@
+from array import array
+
+max_num = 1000
+
+#MET information
+
+met = array('f', [0.])
+met_phi = array('f', [0.])
+met_eta = array('f', [0.])
+
+#Jet information
+
+nJet = array('i', [0])
+minPhi_jetMET = array('f', [0.])
+jet_pt = array('f', max_num*[0.]) 
+jet_energy = array('f', max_num*[0.]) 
+jet_eta = array('f', max_num*[0.]) 
+jet_phi = array('f', max_num*[0.])
+jet_px = array('f', max_num*[0.])
+jet_py = array('f', max_num*[0.])
+jet_pz = array('f', max_num*[0.])
+
+#Electron information
+
+nElectron = array('i', [0])
+electron_pt = array('f', max_num*[0.]) 
+electron_energy = array('f', max_num*[0.]) 
+electron_eta = array('f', max_num*[0.]) 
+electron_phi = array('f', max_num*[0.])
+
+#Muon information
+
+nMuon = array('i', [0])
+muon_pt = array('f', max_num*[0.]) 
+muon_energy = array('f', max_num*[0.]) 
+muon_eta = array('f', max_num*[0.]) 
+muon_phi = array('f', max_num*[0.])
+
+#Tau information
+
+nTau = array('i', [0])
+tau_pt = array('f', max_num*[0.]) 
+tau_energy = array('f', max_num*[0.]) 
+tau_eta = array('f', max_num*[0.]) 
+tau_phi = array('f', max_num*[0.])
+
+#Gen-particles
+
+nParticles = array('i', [0])
+pdgId = array('i', max_num*[0])
+
+#2018 DiJet Triggers
+
+HLT_DiJet110_35_Mjj650_PFMET110_v9 = array('i', [0])
+HLT_DiJet110_35_Mjj650_PFMET120_v9 = array('i', [0])
+HLT_DiJet110_35_Mjj650_PFMET130_v9 = array('i', [0])
+
+def declare_branches(tree):
+	
+	print('######## Creating branches ########')
+
+	tree.Branch('met', met, 'met/F')
+	tree.Branch('met_phi', met_phi, 'met_phi/F')
+	tree.Branch('met_eta', met_eta, 'met_eta/F')
+	
+	tree.Branch('nJet', nJet, 'nJet/I')
+	tree.Branch('minPhi_jetMET', minPhi_jetMET, 'minPhi_jetMET/F')
+	tree.Branch('jet_pt', jet_pt, 'jet_pt[nJet]/F')
+	tree.Branch('jet_energy', jet_energy, 'jet_energy[nJet]/F')
+	tree.Branch('jet_eta', jet_eta, 'jet_eta[nJet]/F')
+	tree.Branch('jet_phi', jet_phi, 'jet_phi[nJet]/F')
+	tree.Branch('jet_px', jet_px, 'jet_px[nJet]/F')	
+	tree.Branch('jet_py', jet_py, 'jet_py[nJet]/F')	
+	tree.Branch('jet_pz', jet_pz, 'jet_pz[nJet]/F')	
+
+	tree.Branch('nElectron', nElectron, 'nElectron/I')
+	tree.Branch('electron_pt', electron_pt, 'electron_pt[nElectron]/F')
+	tree.Branch('electron_phi', electron_phi, 'electron_phi[nElectron]/F')	
+	tree.Branch('electron_eta', electron_eta, 'electron_eta[nElectron]/F')
+	tree.Branch('electron_energy', electron_energy, 'electron_energy[nElectron]/F')
+	
+	tree.Branch('nMuon', nMuon, 'nMuon/I')
+	tree.Branch('muon_pt', muon_pt, 'muon_pt[nMuon]/F')
+	tree.Branch('muon_phi', muon_phi, 'muon_phi[nMuon]/F')
+	tree.Branch('muon_eta', muon_eta, 'muon_eta[nMuon]/F')
+	tree.Branch('muon_energy', muon_energy, 'muon_energy[nMuon]/F')
+
+	tree.Branch('nTau', nTau, 'nTau/I')
+	tree.Branch('tau_pt', tau_pt, 'tau_pt[nTau]/F')
+	tree.Branch('tau_phi', tau_phi, 'tau_phi[nTau]/F')
+	tree.Branch('tau_eta', tau_eta, 'tau_eta[nTau]/F')
+	tree.Branch('tau_energy', tau_energy, 'tau_energy[nTau]/F')
+
+	tree.Branch('nParticles', nParticles, 'nParticles/I')
+	tree.Branch('pdgId', pdgId, 'pdgId[nParticles]/I')
+
+	tree.Branch('HLT_DiJet110_35_Mjj650_PFMET110_v9', HLT_DiJet110_35_Mjj650_PFMET110_v9, 'HLT_DiJet110_35_Mjj650_PFMET110_v9/I')
+	tree.Branch('HLT_DiJet110_35_Mjj650_PFMET120_v9', HLT_DiJet110_35_Mjj650_PFMET120_v9, 'HLT_DiJet110_35_Mjj650_PFMET120_v9/I')
+	tree.Branch('HLT_DiJet110_35_Mjj650_PFMET130_v9', HLT_DiJet110_35_Mjj650_PFMET130_v9, 'HLT_DiJet110_35_Mjj650_PFMET130_v9/I')
+
+	print('######## Branches declared ########')
+
+
+	
+
+
+
+
+
+ 
+ 
+
+
---
+++ writeTree_2018MiniAOD.py
@@ -0,0 +1,219 @@
+import ROOT
+import time
+import argparse
+from math import pi 
+
+from vbf_tree_2018 import * 
+
+# load FWLite C++ libraries
+ROOT.gSystem.Load("libFWCoreFWLite.so");
+ROOT.gSystem.Load("libDataFormatsFWLite.so");
+ROOT.FWLiteEnabler.enable()
+
+# load FWlite python libraries
+from DataFormats.FWLite import Handle, Events	
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--test', help = 'Only go over the first 100 events for testing', action = 'store_true')
+args = parser.parse_args()
+
+def minJetMETPhi(jets, mets):
+	jets_ = jets.product()
+	met = mets.product()[0]
+	phiDiffList = []	
+
+	if len(jets_) <= 4:
+		for j in jets_:
+			phi_diff = abs(j.phi() - met.phi())
+			
+			if phi_diff <= pi:
+				phiDiffList.append(phi_diff)
+
+			else:
+				phiDiffList.append(2*pi - phi_diff)
+
+	else:
+		for i in range(4): #Take only the first four leading jets
+			phi_diff = abs(jets_[i].phi() - met.phi())
+			
+			if phi_diff <= pi:
+				phiDiffList.append(phi_diff)
+
+			else:
+				phiDiffList.append(2*pi - phi_diff)
+
+	return min(phiDiffList)
+
+
+def writeTree(inputFile):
+	
+	#Create a new ROOT file
+	output = ROOT.TFile('VBF_HToInv_2018.root', 'RECREATE')
+
+	#Create a new ROOT TTree
+	eventTree = ROOT.TTree('eventTree', 'eventTree')
+	
+	#Initialize the variables and create branches	
+	declare_branches(eventTree)
+
+	electrons, electronLabel = Handle('std::vector<pat::Electron>'), 'slimmedElectrons'
+	muons, muonLabel = Handle('std::vector<pat::Muon>'), 'slimmedMuons'
+	taus, tauLabel = Handle('std::vector<pat::Tau>'), 'slimmedTaus'
+	photons, photonLabel = Handle('std::vector<pat::Photon>'), 'slimmedPhotons'
+	jets, jetLabel = Handle('std::vector<pat::Jet>'), 'slimmedJets'
+	mets, metLabel = Handle('std::vector<pat::MET>'), 'slimmedMETs'
+	genParticles, genParticlesLabel = Handle('std::vector<reco::GenParticle>'), 'prunedGenParticles'
+
+	triggerBits, triggerBitLabel = Handle("edm::TriggerResults"), ("TriggerResults","","HLT")
+	triggerObjects, triggerObjectLabel  = Handle("std::vector<pat::TriggerObjectStandAlone>"), "slimmedPatTrigger"
+	triggerPrescales, triggerPrescaleLabel  = Handle("pat::PackedTriggerPrescales"), "patTrigger"
+	l1Muons, l1MuonLabel  = Handle("BXVector<l1t::Muon>"), "gmtStage2Digis:Muon"
+	l1EGammas, l1EGammaLabel  = Handle("BXVector<l1t::EGamma>"), "caloStage2Digis:EGamma"
+	l1Jets, l1JetLabel  = Handle("BXVector<l1t::Jet>"), "caloStage2Digis:Jet"
+	l1EtSums, l1EtSumLabel  = Handle("BXVector<l1t::EtSum>"), "caloStage2Digis:EtSum"
+	l1Taus, l1TauLabel  = Handle("BXVector<l1t::Tau>"), "caloStage2Digis:Tau"
+
+	events = Events(inputFile)
+
+	print('Took the input file successfully')
+
+	t1 = time.time()
+
+	for i, event in enumerate(events):
+
+		if args.test:
+			if i == 100: break
+
+		event.getByLabel(electronLabel, electrons)
+		event.getByLabel(muonLabel, muons)
+		event.getByLabel(tauLabel, taus)
+		event.getByLabel(photonLabel, photons)
+		event.getByLabel(jetLabel, jets)
+		event.getByLabel(metLabel, mets)
+		event.getByLabel(genParticlesLabel, genParticles)
+
+		event.getByLabel(triggerBitLabel, triggerBits)
+		event.getByLabel(triggerObjectLabel, triggerObjects)
+		event.getByLabel(triggerPrescaleLabel, triggerPrescales)
+		event.getByLabel(l1MuonLabel, l1Muons)
+		event.getByLabel(l1EGammaLabel, l1EGammas)
+		event.getByLabel(l1JetLabel, l1Jets)
+		event.getByLabel(l1EtSumLabel, l1EtSums)
+		event.getByLabel(l1TauLabel, l1Taus)
+
+		t2 = time.time()
+
+		if i % 100 == 0 and i != 0:		
+			print('Analyzing event # %d , Time: %.2f' % (i , t2-t1))
+	
+		#Storing kinemaic values of interest	
+		
+		met[0] = mets.product()[0].pt()
+		met_phi[0] = mets.product()[0].phi()
+		met_eta[0] = mets.product()[0].eta()
+
+		if met[0] < 50: continue
+
+		jets_ = jets.product()
+
+		nJet[0] = len(jets_)
+
+		for i, jet in enumerate(jets_):
+
+			jet_pt[i] = jet.pt()
+			jet_energy[i] = jet.energy()
+			jet_eta[i] = jet.eta()
+			jet_phi[i] = jet.phi()
+			jet_px[i] = jet.px()
+			jet_py[i] = jet.py()
+			jet_pz[i] = jet.pz()
+
+			minPhi_jetMET[0] = minJetMETPhi(jets, mets) #Minimum delta_phi between jets and MET
+	
+		if jet_pt[0] < 50: continue
+	
+		electrons_ = electrons.product()
+
+		nElectron[0] = len(electrons_)
+	
+		for i, el in enumerate(electrons_):
+	
+			electron_pt[i] = el.pt()
+			electron_eta[i] = el.eta()
+			electron_phi[i] = el.phi()
+		
+		muons_ = muons.product()
+
+		nMuon[0] = len(muons_)
+
+		for i, mu in enumerate(muons_):
+	
+			muon_pt[i] = mu.pt()
+			muon_eta[i] = mu.eta()
+			muon_phi[i] = mu.phi()
+
+		taus_ = taus.product()
+
+		nTau[0] = len(taus_)
+
+		for i, tau in enumerate(taus_):
+	
+			tau_pt[i] = tau.pt()
+			tau_eta[i] = tau.eta()
+			tau_phi[i] = tau.phi()
+
+		genParticles_ = genParticles.product()
+
+		nParticles[0] = len(genParticles_)
+	
+		for i, prt in enumerate(genParticles_):
+
+			pdgId[i] = prt.pdgId()
+			#mothers[i] = prt.mother()
+		
+		triggerBits_ = triggerBits.product()
+
+		names = event.object().triggerNames(triggerBits_)
+	
+		for i in range(triggerBits_.size()):
+
+			if names.triggerNames()[i] == 'HLT_DiJet110_35_Mjj650_PFMET110_v9':
+				if triggerBits_.accept(i):
+					HLT_DiJet110_35_Mjj650_PFMET110_v9[0] = 1
+				else:
+					HLT_DiJet110_35_Mjj650_PFMET110_v9[0] = 0 
+ 
+			elif names.triggerNames()[i] == 'HLT_DiJet110_35_Mjj650_PFMET120_v9':
+				if triggerBits_.accept(i):
+					HLT_DiJet110_35_Mjj650_PFMET120_v9[0] = 1
+				else:
+					HLT_DiJet110_35_Mjj650_PFMET120_v9[0] = 0 
+			
+			elif names.triggerNames()[i] == 'HLT_DiJet110_35_Mjj650_PFMET130_v9':
+				if triggerBits_.accept(i):
+					HLT_DiJet110_35_Mjj650_PFMET130_v9[0] = 1
+				else:
+					HLT_DiJet110_35_Mjj650_PFMET130_v9[0] = 0 
+		
+		eventTree.Fill()
+
+	#Save the output root file
+	output.Write()
+
+if __name__ == '__main__':
+
+	inputFile = 'root://cmsxrootd.fnal.gov///store/mc/RunIIAutumn18MiniAOD/VBF_HToInvisible_M125_TuneCP5_PSweights_13TeV_powheg_pythia8/MINIAODSIM/102X_upgrade2018_realistic_v15-v1/80000/FDBE8CDB-175D-D942-8046-37E10DD9D6CE.root'
+
+	writeTree(inputFile)
+
+	
+
+
+
+
+
+
+
+
+
+

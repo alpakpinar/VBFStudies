5b6d1346561438d80bf2a683187f500d0bb2efd5
Alp Akpinar <aakpinar@bu.edu>
2019-07-05 13:48:46 -0500

    Added contains_bJet flag to 2018 files

--- writeTree_2018MiniAOD.py
+++ writeTree_2018MiniAOD.py
@@ -156,8 +156,31 @@ def writeTree(inputFile):
 	
 					jet_btag_CSVv2[i] = tag.second
 			
-			minPhi_jetMET[0] = minJetMETPhi(jets, mets) #Minimum delta_phi between jets and MET
-	
+		num_bJets = 0
+
+		for val in jet_btag_CSVv2:
+
+			if val > 0.8484: #2017 requirements
+
+				num_bJets += 1 
+
+		if num_bJets != 0:
+			
+			contains_bJet = 1
+
+		else: contains_bJet = 0
+
+		try:
+
+			absEtaDiff_leadingTwoJets[0] = abs(jet_eta[0] - jet_eta[1])
+
+		except IndexError:
+
+			print('Event contains less than 2 jets!')
+			absEtaDiff_leadingTwoJets[0] = 0
+		
+		minPhi_jetMET[0] = minJetMETPhi(jets, mets) #Minimum delta_phi between jets and MET
+		
 		if jet_pt[0] < 50: continue
 	
 		############################

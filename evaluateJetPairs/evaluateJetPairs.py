1b3172f75bb8e50bc6f7870f247ba93febb7fdcb
Alp Akpinar <aakpinar@bu.edu>
2019-07-29 13:43:24 -0500

    Added additional lists to store leading/trailing jet pts for all cases

--- evaluateJetPairs/evaluateJetPairs.py
+++ evaluateJetPairs/evaluateJetPairs.py
@@ -1,4 +1,6 @@
 import ROOT
+from numpy import arange
+from matplotlib import pyplot as plt
 
 # load FWLite C++ libraries
 ROOT.gSystem.Load("libFWCoreFWLite.so");
@@ -98,9 +100,11 @@ def count():
 	mjjValues_leadingPair = [] #Stores max mjj for the case where max mjj comes from leading two pairs
 	mjjValues_otherMaxPair = [] #Stores max mjj for the case where max mjj comes from other combos
 
-	ptValues1_leadingPair = [] #Stores jet_pt[0] for the case where max mjj comes from leading two pairs
-	ptValues2_leadingPair = [] #Stores jet_pt[1] for the case where max mjj comes from leading two pairs
+	ptValues1_leadingPair = [] #Stores jet_pt[0] for the case where max mjj comes from leading two pairs 
+	ptValues2_leadingPair = [] #Stores jet_pt[1] for the case where max mjj comes from leading two pairs 
 
+	leadingJetPtValues_otherMaxPair = [] #Stores jet_pt[0] for the case where max mjj comes from other combos
+	trailingJetPtValues_otherMaxPair = [] #Stores jet_pt[1] for the case where max mjj comes from other combos
 	ptValues1_otherMaxPair = [] #Stores jet_pt of jet with larger pt in the case where this pair have the max mjj 
 	ptValues2_otherMaxPair = [] #Stores jet_pt of jet with smaller pt in the case where this pair have the max mjj 
 
@@ -116,6 +120,8 @@ def count():
 	mother_dict['mjjValues_otherMaxPair'] = mjjValues_otherMaxPair
 	mother_dict['ptValues1_leadingPair'] = ptValues1_leadingPair
 	mother_dict['ptValues2_leadingPair'] = ptValues2_leadingPair
+	mother_dict['leadingJetPtValues_otherMaxPair'] = leadingJetPtValues_otherMaxPair
+	mother_dict['trailingJetPtValues_otherMaxPair'] = trailingJetPtValues_otherMaxPair
 	mother_dict['ptValues1_otherMaxPair'] = ptValues1_otherMaxPair	
 	mother_dict['ptValues2_otherMaxPair'] = ptValues2_otherMaxPair	
 
@@ -124,6 +130,8 @@ def count():
 	#####################
 
 	for iev, event in enumerate(events):
+
+		if iev == 10: break
 
 		if iev % 1000 == 0:
 			print('Working on event {}'.format(iev))
@@ -177,7 +185,7 @@ def count():
 				AK4_tightJets.append(jet) 
 
 		nJet = len(AK4_tightJets)
-		
+
 		if nJet < 2: continue #Discard the events with number of jets smaller than 2
 
 		mjj_values = invMassJetCombos(AK4_tightJets) #Get all the mjj values for all possible combos
@@ -212,6 +220,9 @@ def count():
 			counter_otherCombos[case] += 1
 			mjjValues_otherMaxPair.append(mjj_values['otherCombos'][maxCombo])
 
+			leadingJetPtValues_otherMaxPair.append(AK4_tightJets[0].pt())
+			trailingJetPtValues_otherMaxPair.append(AK4_tightJets[1].pt())
+
 			jet_idx1, jet_idx2 = maxCombo[0], maxCombo[1]
 			
 			if AK4_tightJets[jet_idx1].pt() > AK4_tightJets[jet_idx2].pt():
@@ -227,6 +238,28 @@ def count():
 
 	return mother_dict 
 		
+def createHisto(data):
+
+	'''
+	Creates a histogram out of the given data.
+	Uses matplotlib to construct the histogram.
+	'''
+	#####################
+	#ONLY WORKS FOR MJJ NOW
+	#####################
+
+	bin_width = (max(data)-min(data))/100
+	bins = arange(min(data)-5, max(data)+5, bin_width)
+		
+	plt.xlim([min(data)-5, max(data)+5])
+
+	plt.hist(data, bins=bins)
+	plt.title('Mjj distribution')
+	plt.xlabel('mjj (GeV)')
+	plt.ylabel('Number of Events')
+
+	plt.show()
+
 def main():
 
 	mother_dict = count()
@@ -237,6 +270,8 @@ def main():
 	mjjValues_otherMaxPair = mother_dict['mjjValues_otherMaxPair']
 	ptValues1_leadingPair = mother_dict['ptValues1_leadingPair']
 	ptValues2_leadingPair = mother_dict['ptValues2_leadingPair']
+	leadingJetPtValues_otherMaxPair = mother_dict['leadingJetPtValues_otherMaxPair']
+	trailingJetPtValues_otherMaxPair = mother_dict['trailingJetPtValues_otherMaxPair']
 	ptValues1_otherMaxPair = mother_dict['ptValues1_otherMaxPair']
 	ptValues2_otherMaxPair = mother_dict['ptValues2_otherMaxPair']
 
@@ -255,12 +290,7 @@ def main():
 
 	print('*'*10)
 
-	print(mjjValues_leadingPair)
-	print(mjjValues_otherMaxPair)
-	print(ptValues1_leadingPair)
-	print(ptValues2_leadingPair)
-	print(ptValues1_otherMaxPair)
-	print(ptValues2_otherMaxPair)
+	#createHisto(mjjValues_leadingPair)
 	
 if __name__ == '__main__':
 
